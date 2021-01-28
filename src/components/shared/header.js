e9f24b5c3fe7b03c48129ca6fca8e945a35501ef
Claudia Rivera <riveramacedoc@gmail.com>
2021-01-28 11:20:35 -0500

    urlx2

--- src/components/shared/header.js
+++ src/components/shared/header.js
@@ -17,7 +17,7 @@ export default class Header extends Component{
                                 <NavLink to='/' className="nav__link" exact activeClassName="nav__link--active">Home</NavLink> 
                             </li>
                             <li className="nav__item">
-                                <NavLink to={process.env.PUBLIC_URL + '/about'} className="nav__link" activeClassName="nav__link--active">About</NavLink> 
+                                <NavLink to='about' className="nav__link" activeClassName="nav__link--active">About</NavLink> 
                             </li>
                             <li className="nav__item nav__item--right">
                                 <a className="nav__link nav__icon" target="_blank" rel="noreferrer" href="https://github.com/claudiaarivera"><AiFillGithub/></a>
